28c49f1b94a96ca54f94b7afa21b0571da97355c
Shivaprasad Koyyada <111137636+Shivaprasad431@users.noreply.github.com>
2024-10-05 20:34:39 +0530

    Add files via upload

--- question1.py
+++ question1.py
@@ -24,23 +24,72 @@ def calculate_total_size(file_system, path):
     return f"Total size: {total_size}"
 
 # Example file system structure
-file_system = {
+filesystem = {
     "root": {
         "dir1": {
             "subdir1": {
-                "file1": {"size": 500},
-                "file2": {"size": 500},
+                "file1.txt": 100,
+                "file2.txt": 200,
+                "subsubdir1": {
+                    "file3.txt": 50,
+                    "file4.txt": 150,
+                    "subsubsubdir1": {
+                        "file5.txt": 500,
+                        "emptydir1": {}
+                    }
+                }
             },
-            "subdir2": {},
+            "subdir2": {
+                "file6.txt": 300,
+                "subsubdir2": {
+                    "file7.txt": 700,
+                    "subsubsubdir2": {
+                        "file8.txt": 800,
+                        "file9.txt": 900
+                    }
+                },
+                "emptydir2": {}
+            },
+            "file10.txt": 1000
         },
         "dir2": {
-            "subdir4": {
-                "subsubdir5": {
-                    "file3": {"size": 1200},
-                    "file4": {"size": 2200},
+            "subdir3": {
+                "subsubdir3": {
+                    "file11.txt": 400,
+                    "file12.txt": 500
                 },
+                "subsubdir4": {
+                    "emptydir3": {}
+                }
             },
+            "subdir4": {
+                "file13.txt": 600,
+                "subsubdir5": {
+                    "file14.txt": 700,
+                    "file15.txt": 800,
+                    "subsubsubdir3": {
+                        "emptydir4": {},
+                        "file16.txt": 900,
+                        "file17.txt": 1000
+                    }
+                }
+            },
+            "emptydir5": {}
         },
+        "dir3": {
+            "file18.txt": 1100,
+            "subdir5": {
+                "subsubdir6": {
+                    "file19.txt": 1200,
+                    "subsubsubdir4": {
+                        "file20.txt": 1300,
+                        "emptydir6": {}
+                    }
+                }
+            }
+        },
+        "emptydir7": {},
+        "file21.txt": 1400
     }
 }
 
